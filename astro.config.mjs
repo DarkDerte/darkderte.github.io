3ba9a2a13236ad83626bea834033389b04141ec7
DarkDerte <darkderte@gmail.com>
2024-08-11 02:38:53 +0200

    blog start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
-import icon from "astro-icon";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
